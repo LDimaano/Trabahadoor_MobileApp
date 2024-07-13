a0d7903641a96cd22966ca16983604e1b3f5e4cf
LDimaano <21-09169@g.batstate-u.edu.ph>
2024-07-13 12:43:01 +0800

    change default fonts to poppins

--- trabahadoor/lib/main.dart
+++ trabahadoor/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 // import 'package:trabahadoor/screens/chat/HomeChat.dart';
 // import 'package:trabahadoor/screens/employer_side/employer.dart';
 // import 'package:trabahadoor/screens/profile/profile.dart';
@@ -24,6 +25,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primaryColor: const Color.fromRGBO(3, 63, 118, 1),
           hintColor: const Color(0x0ffed408),
+          textTheme: GoogleFonts.poppinsTextTheme(),
         ),
         home: const MainScreen());
   }
