45450520c20e30d8a55236dd01d5b4be8f87e2a4
carla-dimaandal <carlafrancescadimaandal29@gmail.com>
2024-07-16 10:56:32 +0800

    adjust profile

--- trabahadoor/lib/screens/profile/profile.dart
+++ trabahadoor/lib/screens/profile/profile.dart
@@ -13,72 +13,96 @@ class ProfilePage extends StatelessWidget {
       ),
       backgroundColor: Colors.white.withOpacity(0.95),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child: CircleAvatar(
-                  radius: 50,
-                  backgroundImage: AssetImage('asset/images/profile.jpg'),
-                ),
-              ),
-              const SizedBox(height: 10),
-              Center(
-                child: Text(
-                  'Jisoo',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-              ),
-              const SizedBox(height: 5),
-              Center(
-                child: Text(
-                  'Jisoo@gmail.com',
-                  style: TextStyle(color: Colors.grey),
-                ),
-              ),
-              const SizedBox(height: 20),
-              Center(
-                child: ElevatedButton.icon(
-                  onPressed: () {},
-                  icon: const Icon(Icons.edit, size: 20, color: Colors.white),
-                  label: const Text(
-                    'Edit Profile',
-                    style: TextStyle(color: Colors.white),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 10),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _buildProfilePicture(), // Display profile picture here
+                      const SizedBox(width: 20),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Jisoo',
+                            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 5),
+                          Text(
+                            'Jisoo@gmail.com',
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                          const SizedBox(height: 20),
+                          Center(
+                            child: ElevatedButton.icon(
+                              onPressed: () {},
+                              icon: const Icon(Icons.edit, size: 20, color: Colors.white),
+                              label: const Text(
+                                'Edit Profile',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Theme.of(context).primaryColor,
+                                shape: const StadiumBorder(),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Theme.of(context).primaryColor,
-                    shape: const StadiumBorder(),
+                  const SizedBox(height: 30),
+                  _buildSectionTitle('Work Experience'),
+                  _buildExperienceItem(
+                    'Systems Analyst',
+                    'Company ABC',
+                    'January 2018 - December 2020',
+                    'Led a team of analysts in developing and implementing systems for data management.',
                   ),
-                ),
+                  _buildExperienceItem(
+                    'Full-Stack Developer',
+                    'Tech Solutions Inc.',
+                    'May 2016 - December 2017',
+                    'Designed and developed scalable web applications using React.js and Node.js.',
+                  ),
+                  _buildExperienceItem(
+                    'Mobile App Developer',
+                    'MobileTech Ltd.',
+                    'August 2014 - April 2016',
+                    'Created native Android applications that achieved 1 million downloads on Google Play.',
+                  ),
+                  const SizedBox(height: 20),
+                  _buildSectionTitle('Skills'),
+                  _buildSkillList(),
+                ],
               ),
-              const SizedBox(height: 20),
-              _buildSectionTitle('Skills'),
-              _buildSkillList(),
-              const SizedBox(height: 20),
-              _buildSectionTitle('Experience'),
-              _buildExperienceItem(
-                'Systems Analyst',
-                'Company ABC',
-                'January 2018 - December 2020',
-                'Led a team of analysts in developing and implementing systems for data management.',
-              ),
-              _buildExperienceItem(
-                'Full-Stack Developer',
-                'Tech Solutions Inc.',
-                'May 2016 - December 2017',
-                'Designed and developed scalable web applications using React.js and Node.js.',
-              ),
-              _buildExperienceItem(
-                'Mobile App Developer',
-                'MobileTech Ltd.',
-                'August 2014 - April 2016',
-                'Created native Android applications that achieved 1 million downloads on Google Play.',
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildProfilePicture() {
+    return Container(
+      padding: const EdgeInsets.all(3),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: Colors.blueGrey.withOpacity(0.5), // Blue border color
+          width: 5, // Adjust border width as needed
+        ),
+      ),
+      child: CircleAvatar(
+        radius: 50,
+        backgroundImage: AssetImage('asset/images/profile.jpg'),
       ),
     );
   }
