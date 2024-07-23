51276fc30bcf354635cad9cb589ec0115ff2f88c
LDimaano <21-09169@g.batstate-u.edu.ph>
2024-07-23 09:08:01 +0800

    add chat empside

---
+++ trabahadoor/lib/screens/chat_empside/ChatPage_empside.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:trabahadoor/screens/chat/widgets/ChatBottomSheet.dart';
+import 'package:trabahadoor/screens/chat/widgets/ChatSample.dart';
+
+class ChatPage extends StatelessWidget {
+  const ChatPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(70.0),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 5),
+          child: AppBar(
+            leadingWidth: 30,
+            title: Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(30),
+                  child: Image.asset(
+                    "asset/images/chat1.jpg",
+                    height: 45,
+                    width: 45,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 10),
+                  child: Text(
+                    "Jihyo Park",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+            actions: const [
+              Padding(
+                padding: EdgeInsets.only(right: 25),
+                child: Icon(Icons.call,
+                    color: Color.fromRGBO(3, 63, 118, 1), size: 26),
+              ),
+              Padding(
+                padding: EdgeInsets.only(right: 25),
+                child: Icon(Icons.video_call,
+                    color: Color.fromRGBO(3, 63, 118, 1), size: 30),
+              ),
+              Padding(
+                padding: EdgeInsets.only(right: 25),
+                child:
+                    Icon(Icons.more_vert, color: Color.fromRGBO(3, 63, 118, 1)),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: ListView(
+        padding:
+            const EdgeInsets.only(top: 20, left: 20, right: 20, bottom: 80),
+        children: const [
+          ChatSample(),
+          ChatSample(),
+          ChatSample(),
+          ChatSample(),
+          ChatSample(),
+        ],
+      ),
+      bottomSheet: const ChatBottomSheet(),
+    );
+  }
+}
---
+++ trabahadoor/lib/screens/chat_empside/HomeChat_empside.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:trabahadoor/screens/chat/widgets/ActiveChats.dart';
+import 'package:trabahadoor/screens/chat/widgets/RecentChats.dart';
+
+class Homechat extends StatelessWidget {
+  const Homechat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Messages', style: TextStyle(color: Colors.white)),
+        backgroundColor: Theme.of(context).primaryColor,
+        elevation: 0,
+      ),
+      backgroundColor:
+          const Color.fromARGB(255, 217, 211, 211).withOpacity(0.1),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: ListView(
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 15),
+            ),
+            const SizedBox(height: 1),
+            Container(
+              height: 50,
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(left: 8.0),
+                    child: Icon(
+                      Icons.search,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  const SizedBox(width: 2),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 10.0),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            hintText: "Search",
+                            hintStyle: TextStyle(color: Colors.grey),
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 5),
+            ActiveChats(),
+            RecentChats(),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: const Color.fromRGBO(3, 63, 118, 1),
+        child: const Icon(
+          Icons.message,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
